a49e0b21ed0e7bf44b78cdfc08e6f1a1fd9b221d
Blesass <mblesapardo1@gmail.com>
2025-05-26 05:20:27 +0200

    Nueva gestión de token. Admin y padre durante la sesión, no localStorage Niños según el limite del padre, si localStorage

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,5 +1,7 @@
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework_simplejwt.tokens import AccessToken
+from datetime import timedelta
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -39,20 +41,34 @@ class LoginView(APIView):
 
     def post(self, request):
         from django.contrib.auth import authenticate
-        from rest_framework_simplejwt.tokens import RefreshToken
 
         username = request.data.get('username')
         password = request.data.get('password')
         user = authenticate(username=username, password=password)
+
         if user and user.is_active:
-            refresh = RefreshToken.for_user(user)
+            # Tiempo por defecto para admin/padre
+            tiempo_token = timedelta(minutes=60)
+
+            if user.is_infantil:
+                try:
+                    config = ConfiguracionParental.objects.get(usuario=user.padre)
+                    minutos = config.limite_tiempo or 30  # fallback si es None
+                    tiempo_token = timedelta(minutes=minutos)
+                except ConfiguracionParental.DoesNotExist:
+                    tiempo_token = timedelta(minutes=30)
+
+            # Generar el token manualmente
+            access = AccessToken.for_user(user)
+            access.set_exp(from_time=None, lifetime=tiempo_token)
+
             return Response({
-                'access_token': str(refresh.access_token),
-                'refresh_token': str(refresh),
+                'access_token': str(access),
                 'username': user.username,
                 'es_padre': user.es_padre,
                 'es_infantil': user.es_infantil
             })
+
         return Response({"error": "Credenciales inválidas"}, status=status.HTTP_401_UNAUTHORIZED)
 
 class ConfiguracionParentalView(APIView):
